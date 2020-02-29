1abf540d1fb0cb9124701ab3c76d5975148fc8a5
Albert Kozak <albert.kozak@live.com>
2020-02-28 19:34:13 -0800

    Added category picker

--- CoffeeCulprit/CoffeeCulprit/ViewController.swift
+++ CoffeeCulprit/CoffeeCulprit/ViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 import ArcGIS
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, AGSGeoViewTouchDelegate {
+  
+  private typealias Category = (title: String, color: UIColor)
+  private let categories:
+    [Category] = [
+      ("Coffee shop", .brown),
+      ("Gas station", .orange),
+      ("Food", .cyan), ("Hotel", .blue),
+      ("Neighborhood", .black),
+      ("Parks and Outdoors", .green)]
 
   @IBOutlet weak var mapView: AGSMapView!
+  @IBOutlet weak var categoryPicker: UIPickerView!
   
   private func setupMap() {
       mapView.map = AGSMap(basemapType: .navigationVector, latitude: 49.282, longitude: -123.1171, levelOfDetail: 13)
