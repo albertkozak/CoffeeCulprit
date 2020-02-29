9812caacbe3d0d3c1c436f8c909d66de70dd9b57
Albert Kozak <albert.kozak@live.com>
2020-02-28 19:41:18 -0800

    Added data source + delegate protocol methods

--- CoffeeCulprit/CoffeeCulprit/ViewController.swift
+++ CoffeeCulprit/CoffeeCulprit/ViewController.swift
@@ -87,6 +87,22 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     setupMap()
   }
   
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+      return 1
+  }
+
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+      return categories.count
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+      return categories[row].title
+  }
+
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+      findPlaces(forCategory: categories[row])
+  }
+  
 
 
 }
