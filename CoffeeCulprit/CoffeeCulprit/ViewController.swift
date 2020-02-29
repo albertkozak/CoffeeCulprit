533cc98b2ac558b4c3a10c8657cfb63317c7bcd9
Albert Kozak <albert.kozak@live.com>
2020-02-28 18:21:10 -0800

    Connected to ArcGIS SDK, added ASGMapView with Vancouver coordinates

--- CoffeeCulprit/CoffeeCulprit/ViewController.swift
+++ CoffeeCulprit/CoffeeCulprit/ViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import UIKit
+import ArcGIS
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var mapView: AGSMapView!
+  
+  private func setupMap() {
+      mapView.map = AGSMap(basemapType: .navigationVector, latitude: 49.282, longitude: -123.1171, levelOfDetail: 13)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    setupMap()
   }
+  
 
 
 }
